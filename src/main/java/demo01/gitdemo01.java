134028cc7a727731ab136bdc90c02ffb9bd2a924
Wucc <1944731848@qq.com>
2023-08-23 16:25:23 +0800

    hot fix 第一次提交

--- src/main/java/demo01/gitdemo01.java
+++ src/main/java/demo01/gitdemo01.java
@@ -4,5 +4,6 @@ public class gitdemo01 {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git01");
+        System.out.println("git hot fix");
     }
 }
